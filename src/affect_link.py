91572c4687ec5a6ad8b7714a227e549471c0112a
Edmon Begoli <xem@ornl.gov>
2012-04-05 20:03:25 -0400

    modifications for wordnet query on emotions

--- src/affect_link.py
+++ src/affect_link.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 '''
  get the POS and link it with wordnet, wordnet-affect
@@ -8,32 +9,8 @@ __author__ = 'Edmon Begoli'
 from urllib import urlopen
 from nltk import *
 from nltk.corpus import wordnet as wn
-from StringIO import StringIO
-from zipfile import ZipFile
-from pymongo import Connection
-
-#mongodb://<user>:<password>@dbh45.mongolab.com:27457/asd-text
-
-conn = Connection('dbh45.mongolab.com',27457 ) 
-db = conn['asd-text']
-db.authenticate( 'asdadmin','asdadmin' )
-
 
 link = "http://legacy.c-span.org/newsmakers/schu.htm"
-
-print db.collection_names()
-
-def get_childes():
-
-	url_dir = 'http://childes.psy.cmu.edu/data/Clinical/'
-	listing_html = urlopen(url_dir).read()
-	zip_files = [ str(url_dir + zip_file) for zip_file in nltk.clean_html( listing_html ).split(' ') if str(zip_file).endswith('.zip')]
-	print 'test'
-
-	for zip_file_url in zip_files:
-		print 'examining ' + zip_file_url
-        zipfile = ZipFile(StringIO(urlopen( zip_file_url ).read()))
-        print zipfile.namelist()  
 
 def main():
 	print "something"
@@ -48,4 +25,4 @@ def main():
 		print wn.synsets( pos[0] )
 
 if __name__ == '__main__':
-	pass
+	main()
--- src/emotions.py
+++ src/emotions.py
@@ -7,14 +7,14 @@ parrot_primary = {}
 secondary = {}
 
 from nltk.corpus import wordnet as wn
-
+from io import FileIO
 
 #For love
 secondary['affection'] = ('adoration', 'affection', 'love', 'fondness', 'liking'
                           , 'attraction', 'caring', 'tenderness', 
                           'compassion', 'sentimentality')
 secondary['lust'] = ('arousal', 'desire', 'lust', 'passion', 'infatuation')
-secondary['longing'] = ('longing')
+secondary['longing'] = list('longing')
 
 parrot_primary['love'] = dict( (i,secondary[i]) for i in ('affection', 'lust',
                                                            'longing') )
@@ -30,7 +30,7 @@ secondary['contentment'] = ( 'contentment', 'pleasure' )
 secondary['pride'] = ('pride', 'triumph')
 secondary['optimism'] = ('eagerness', 'hope', 'optimism')
 secondary['enthrallment'] = ('enthrallment', 'rapture')
-secondary['relief'] = ('relief')
+secondary['relief'] = list('relief')
 
 parrot_primary['joy'] = dict( (i,secondary[i]) for i in ('cheerfulness',
                                                          'zest', 'contentment', 
@@ -51,7 +51,7 @@ secondary['rage'] = ( 'anger', 'rage', 'outrage', 'fury', 'wrath', 'hostility',
                       'spite', 'vengefulness', 'dislike', 'resentment' )
 secondary['disgust'] = ( 'disgust', 'revulsion', 'contempt' )
 secondary['envy'] = ( 'envy', 'jealousy' )
-secondary['torment'] = ('torment')
+secondary['torment'] = list('torment')
 
 parrot_primary['anger'] = dict( (i,secondary[i]) for i in ('irritation',
                                'exasperation', 'rage',
@@ -80,7 +80,29 @@ secondary['nervousness'] = ( 'anxiety', 'nervousness', 'tenseness', 'uneasiness'
 
 parrot_primary['fear'] = dict( (i,secondary[i]) for i in ('fear', 'nervousness') )
 
-def pretty(d, indent=0):
+def get_synsets(d, indent=0):
+    with open("parrot_synsets.txt", "w") as f:
+
+      for prim, value in d.iteritems():
+          print ' for primary: ' + prim
+          f.write( '\n primary: ' + prim + "\n" )
+          for syn in wn.synsets( prim ):
+            print str(syn)
+            f.write(  str(syn) + " " )
+          for sec, val in value.iteritems():
+              print ' for secondary: ' + sec
+              f.write( '\nsecondary: ' + sec + "\n" )
+              for syn in wn.synsets( sec ):
+                f.write( str(syn) + " " )
+              for ter in val: 
+                print 'for tertiary: ' + ter
+                f.write( '\ntertiary: ' + ter + "\n" )
+                for syn in wn.synsets( ter ):
+                    print str(syn)
+                    f.write( str(syn) + " ")
+
+
+def pretty_print( d, indent=0 ):
     for key, value in d.iteritems():
         print '\t' + key
         #print '\t' + str(value)
@@ -88,8 +110,10 @@ def pretty(d, indent=0):
             print '\t\t' + k
             for em in val:
                 print '\t\t\t' + em
+
+
 def main():
-    pretty( parrot_primary)
+    get_synsets( parrot_primary)
     
 if __name__ == "__main__":
     main()    
